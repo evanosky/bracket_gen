4acc6a025c9036b7909af29ccf76d87744a667c9
Evan Dietz <evan.r.dietz@gmail.com>
2013-03-01 21:55:21 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BracketGen::Application.config.secret_token = '5725456a96c02a42383d4bad5bb6ea178227587d6ca7a64d8c683b7ad5590f94eb0586d5aede73580ba70750404566bb69f7b861ea87aff94e4a65ff44a360c3'
